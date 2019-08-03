ced69f58214ff386bb119931e0dd59ab389e9728
MyPoZi <kxx555u.u@gmail.com>
2019-08-03 17:50:10 +0900

    [add] itemsのcreateメソッドを追加

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -6,6 +6,21 @@ module Api
         items = Item.all
         render json: { status: 'SUCCESS', message: 'loaded items', data: items }
       end
+
+      def create
+        item = Item.new(item_params)
+        if item.save
+          render json: { status: 'SUCCESS', message: 'loaded the post', data: item }
+        else
+          render json: { status: 'SUCCESS', message: 'loaded the post', data: item.errors }
+        end
+      end
+
+      private
+
+      def item_params
+        params.require(:item).permit(:title, :description, :price)
+      end
     end
   end
 end
