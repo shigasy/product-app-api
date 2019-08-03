c272863b004beb19846b98fde775c45071d40f98
MyPoZi <kxx555u.u@gmail.com>
2019-08-03 20:19:39 +0900

    [add] itemsテーブルのtitleとpriceにnot nullを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_084504) do
+ActiveRecord::Schema.define(version: 2019_08_03_105955) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", limit: 100
+    t.string "title", null: false
     t.string "description", limit: 500
-    t.integer "price"
+    t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
